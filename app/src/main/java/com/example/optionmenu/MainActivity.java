f5acb37d411b0e36ebf3074b6353731072e97365
park <wasf4294@naver.com>
2023-03-20 21:08:12 +0900

    Menu

---
+++ app/src/main/java/com/example/optionmenu/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.optionmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_option, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.menu1:
+                Toast.makeText(this, "menu select", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu2:
+                Toast.makeText(this, "menu2 select", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
